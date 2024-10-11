2449b7feef9341b9c86b49d34b1413f1a8c00957
ryanomoro37 <ryanomoro37@gmail.com>
2024-10-11 14:29:40 +0000

    update card.js

--- src/Card.js
+++ src/Card.js
@@ -1,32 +1,32 @@
+
 import React from 'react';
-    import './Card.css';
-// Suggested code may be subject to a license. Learn more: ~LicenseLog:3912560361.
-    import {v4 as uuidv4} from 'uuid';
+import './Card.css';
+import { v4 as uuidv4 } from 'uuid';
 
-    export default class Card extends React.Component {
-      getCardClassName() {
-        let className = '';
-        if (this.props.status === 'backlog') {
-          className = 'Card-grey';
-        } else if (this.props.status === 'in-progress') {
-          className = 'Card-blue';
-        } else if (this.props.status === 'complete') {
-          className = 'Card-green';
-        }
-        return className;
-      }
-
-      render() {
-        const uniqueId = this.props.id ? `${this.props.id}-${uuidv4()}` : uuidv4(); 
-
-        return (
-          <div 
-            className={`Card ${this.getCardClassName()}`} 
-            data-id={uniqueId} 
-            data-status={this.props.status} 
-          >
-            <div className="Card-title">{this.props.name}</div>
-          </div>
-        );
-      }
+export default class Card extends React.Component {
+  getCardClassName() {
+    let className = '';
+    if (this.props.status === 'backlog') {
+      className = 'Card-grey';
+    } else if (this.props.status === 'in-progress') {
+      className = 'Card-blue';
+    } else if (this.props.status === 'complete') {
+      className = 'Card-green';
     }
+    return className;
+  }
+
+  render() {
+    const uniqueId = this.props.id ? `${this.props.id}-${uuidv4()}` : uuidv4();
+
+    return (
+      <div 
+        className={`Card ${this.getCardClassName()}`} 
+        data-id={uniqueId} 
+        data-status={this.props.status} 
+      >
+        <div className="Card-title">{this.props.name}</div>
+      </div>
+    );
+  }
+}
